9b9cd8903f33f34412e7b45b33ead23b51f6b982
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2023-03-07 19:12:39 +0000

    player racing added

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,7 @@ fn view(_app: &App, _model: &Model, frame: Frame) {
     };
     let me = Player {
         name: String::from("isaac"),
-        speed: 88.0,
+        speed: 50.0,
         position: Position {
             x: 100.0,
             y: 0.0,
@@ -108,25 +108,21 @@ fn view(_app: &App, _model: &Model, frame: Frame) {
         }
     };
 
-    draw.ellipse().color(WHITE).w(10.0).h(10.0).x_y(tyreek_hill.position.x * 3.0, tyreek_hill.position.y * 3.0);
-    draw.ellipse().color(WHITE).w(10.0).h(10.0).x_y(me.position.x * 3.0, me.position.y * 3.0);
     draw.ellipse().color(WHITE).w(5.0).h(5.0).x_y(ball.position.x * 3.0, ball.position.y * 3.0);
 
-    let sine = _app.time.sin();
-
-
-    let slowersine = (_app.time / 2.0).sin();
-
     // Map the sine wave functions to ranges between the boundaries of the window
-    let x = map_range(1.0, -1.0, 1.0, 0.0, ball.position.x * 3.0);
-    let y = map_range(run(me, _app.time), -1.0, 1.0, 0.0, ball.position.y * 3.0);
+    let x = map_range(run(&me, _app.time), -1.0, 1.0, me.position.x * 3.0, ball.position.x * 3.0);
+    let y = map_range(run(&me, _app.time), -1.0, 1.0, me.position.y * 3.0, ball.position.y * 3.0);
+    draw.ellipse().color(WHITE).w(10.0).h(10.0).x_y(x, y);
 
-    draw.ellipse().color(RED).w(5.0).h(5.0).x_y(x, y);
+    let x_tyreek = map_range(run(&tyreek_hill, _app.time), -1.0, 1.0, tyreek_hill.position.x * 3.0, ball.position.x * 3.0);
+    let y_tyreek = map_range(run(&tyreek_hill, _app.time), -1.0, 1.0, tyreek_hill.position.y * 3.0, ball.position.y * 3.0);
+    draw.ellipse().color(WHITE).w(10.0).h(10.0).x_y(x_tyreek, y_tyreek);
 
     draw.to_frame(_app, &frame).unwrap();
 }
 
-fn run(player: Player, time: f32) -> f32 {
+fn run(player: &Player, time: f32) -> f32 {
     let distance = ((player.speed * 0.01) * time) - 1.0;
 
     if distance > 1.0 {
